fc749fcdebe65651fd2214fc0cca9970815a5a2f
Hagar Elbakry <161838415+Hagar-Elbakry@users.noreply.github.com>
2024-09-20 17:57:09 +0300

    Enhance session security by enabling strict mode and setting secure cookie parameters

--- signup-with-MVC/app/core/config.php
+++ signup-with-MVC/app/core/config.php
@@ -1,17 +1,44 @@
 <?php
 
-if($_SERVER['SERVER_NAME']=='localhost') {
-    // database configuration  
-    define('DBHOST','localhost');
-    define('DBNAME','my_db');
-    define('DBUSER','root');
-    define('DBPASS','');
-    define('ROOT','http://localhost/Projects/signup-with-MVC/public');
+
+define('DBHOST','localhost');
+define('DBNAME','my_db');
+define('DBUSER','root');
+define('DBPASS','');
+
+
+ini_set("session.use_only_cookies", 1);
+ini_set("session.use_strict_mode", 1);
+
+session_set_cookie_params([
+    'lifetime' => 1800,
+    'domain' => "localhost",
+    'path' => '/',
+    'secure' => true,
+    'httponly' =>true
+]);
+
+session_start();
+
+function regenerate_session_id() {
+    session_regenerate_id();
+    $_SESSION['Last_regeneration'] = time();
+}
+if(!isset($_SESSION['Last_regeneration'])) {
+    regenerate_session_id();
 } else {
-    define('DBHOST','localhost');
-    define('DBNAME','my_db');
-    define('DBUSER','root');
-    define('DBPASS','');
+    $interval = 60*30;
+    if(time() - $_SESSION['Last_regeneration'] >= $interval) {
+        regenerate_session_id();
+    }
+}
+
+if($_SERVER['SERVER_NAME']=='localhost') {
+    
+    define('ROOT','http://localhost/Projects/signup-with-MVC/public');
+    
+} else {
+    
     define('ROOT', 'https://www.yourwebsite.com');
 }
 
